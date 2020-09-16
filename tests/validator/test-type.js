1f12981bc0e22c7e1e456777e94cebb47ed8a762
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:26:45 +0100

    Add placeholder type tests

---
+++ tests/validator/test-type.js
@@ -0,0 +1,9 @@
+const test = require('ava');
+
+test.todo('accepts unit type');
+
+test.todo('accepts conjunction type');
+
+test.todo('rejects empty type string');
+
+test.todo('rejects invalid type string');
