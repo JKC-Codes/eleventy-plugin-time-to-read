3204cd3be482b352afc34a78efc7beb3401e27fc
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:41:14 +0100

    Add placeholder keys test

---
+++ tests/validator/test-invalid-keys.js
@@ -0,0 +1,23 @@
+const test = require('ava');
+
+test.todo('accepts speed as key');
+
+test.todo('accepts language as key');
+
+test.todo('accepts style as key');
+
+test.todo('accepts type as key');
+
+test.todo('accepts hours as key');
+
+test.todo('accepts minutes as key');
+
+test.todo('accepts seconds as key');
+
+test.todo('accepts prepend as key');
+
+test.todo('accepts append as key');
+
+test.todo('accepts digits as key');
+
+test.todo('rejects invalid options key');
