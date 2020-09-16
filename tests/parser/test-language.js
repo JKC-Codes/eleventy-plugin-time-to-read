d961991af812eae1c88cec69236ccf4e7c894e60
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:08:20 +0100

    Add placeholder parser tests

---
+++ tests/parser/test-language.js
@@ -0,0 +1,5 @@
+const test = require('ava');
+
+test.todo('accepts valid language option');
+
+test.todo('rejects invalid language option');
---
+++ tests/parser/test-speed.js
@@ -0,0 +1,5 @@
+const test = require('ava');
+
+test.todo('accepts valid speed option');
+
+test.todo('rejects invalid speed option');
