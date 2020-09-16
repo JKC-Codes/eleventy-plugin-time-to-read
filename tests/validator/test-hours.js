76b1cd1b14fcd68e9fd975a1453ea1edda40a601
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:31:42 +0100

    Add placeholder label tests

---
+++ tests/validator/test-hours.js
@@ -0,0 +1,15 @@
+const test = require('ava');
+
+test.todo('accepts hours as True');
+
+test.todo('accepts hours as False');
+
+test.todo('accepts hours as auto');
+
+test.todo('rejects empty hours string');
+
+test.todo('rejects truthy hours');
+
+test.todo('rejects falsy hours');
+
+test.todo('rejects invalid hours string');
---
+++ tests/validator/test-minutes.js
@@ -0,0 +1,15 @@
+const test = require('ava');
+
+test.todo('accepts minutes as True');
+
+test.todo('accepts minutes as False');
+
+test.todo('accepts minutes as auto');
+
+test.todo('rejects empty minutes string');
+
+test.todo('rejects truthy minutes');
+
+test.todo('rejects falsy minutes');
+
+test.todo('rejects invalid minutes string');
