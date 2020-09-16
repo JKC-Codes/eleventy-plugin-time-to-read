19635656dee8607834749e0cbd8f8ae1651d5de7
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:35:42 +0100

    Add placeholder app/prepend tests

---
+++ tests/validator/test-append.js
@@ -0,0 +1,11 @@
+const test = require('ava');
+
+test.todo('accepts append as Null');
+
+test.todo('accepts append as string');
+
+test.todo('accepts append as number');
+
+test.todo('rejects append as array');
+
+test.todo('rejects append as object');
---
+++ tests/validator/test-prepend.js
@@ -0,0 +1,11 @@
+const test = require('ava');
+
+test.todo('accepts prepend as Null');
+
+test.todo('accepts prepend as string');
+
+test.todo('accepts prepend as number');
+
+test.todo('rejects prepend as array');
+
+test.todo('rejects prepend as object');
