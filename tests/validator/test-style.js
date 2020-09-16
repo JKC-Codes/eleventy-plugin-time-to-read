59e298b794e83c981e29bb0609fb5d5a6fbbab2a
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-16 14:24:45 +0100

    Add placeholder style tests

---
+++ tests/validator/test-style.js
@@ -0,0 +1,11 @@
+const test = require('ava');
+
+test.todo('accepts narrow style');
+
+test.todo('accepts short style');
+
+test.todo('accepts long style');
+
+test.todo('rejects empty style string');
+
+test.todo('rejects invalid style string');
