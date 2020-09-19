242aef1808fcf8529f3b8d6a4135f8e8a0947c5e
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-19 15:56:44 +0100

    Change language default from en-GB to just en

--- components/options-default.js
+++ components/options-default.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// The number of characters read per minute tends to be around 1000 for all languages: https://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
 	speed: '1000 characters per minute',
-	language: 'en-GB',
+	language: 'en',
 	style: 'long',
 	type: 'unit',
 	hours: 'auto',
