caf45af6cbdd1ea2a032c57c3a15b3e95918d659
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-21 18:31:45 +0100

    Change notThrows to is

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,76 +2,82 @@ const test = require('ava');
 const parser = require('../components/options-parser.js');
 
 
-test('accepts valid speed arguments', t => {
-	t.notThrows(()=> {
-		parser(['250 words a minute']);
-	});
+test('returns valid speed argument as object', t => {
+	const testArgument = '250 words a minute';
+	t.is(parser([testArgument]).speed, testArgument);
 });
 
-test('accepts valid language arguments', t => {
-	t.notThrows(()=> {
-		parser(['en']);
-	});
+test('returns valid language argument as object', t => {
+	const testArgument = 'en';
+	t.is(parser([testArgument]).language, testArgument);
 });
 
-test('accepts object arguments', t => {
-	t.notThrows(()=> {
-		parser([{
-			speed: '1000 characters per minute',
-			language: 'en',
-			style: 'long',
-			type: 'unit',
-			hours: 'auto',
-			minutes: true,
-			seconds: false,
-			prepend: null,
-			append: null,
-			digits: 1
-		}]);
-	});
+test('returns array of arguments as object', t => {
+	const testArgument = ['1000 characters per hour', 'hi'];
+	t.is(parser(testArgument).speed, testArgument[0]);
+	t.is(parser(testArgument).language, testArgument[1]);
 });
 
-test('rejects invalid speed arguments', t => {
+test('accepts object argument', t => {
+	const testArgument = {
+		speed: '1000 characters per minute',
+		language: 'en',
+		style: 'long',
+		type: 'unit',
+		hours: 'auto',
+		minutes: true,
+		seconds: false,
+		prepend: null,
+		append: null,
+		digits: 1
+	};
+	t.is(parser([testArgument]).speed, testArgument.speed);
+	t.is(parser([testArgument]).language, testArgument.language);
+	t.is(parser([testArgument]).style, testArgument.style);
+	t.is(parser([testArgument]).type, testArgument.type);
+	t.is(parser([testArgument]).hours, testArgument.hours);
+	t.is(parser([testArgument]).minutes, testArgument.minutes);
+	t.is(parser([testArgument]).seconds, testArgument.seconds);
+	t.is(parser([testArgument]).prepend, testArgument.prepend);
+	t.is(parser([testArgument]).append, testArgument.append);
+	t.is(parser([testArgument]).digits, testArgument.digits);
+});
+
+test('rejects invalid speed argument', t => {
 	t.throws(() => {
-		parser(['99 chars min'])
+		parser(['0 words a minute'])
+	});
+	t.throws(() => {
+		parser(['250 chars a minute'])
+	});
+	t.throws(() => {
+		parser(['250 words a min'])
 	});
 });
 
-test('rejects invalid language arguments', t => {
+test('rejects invalid language argument', t => {
 	t.throws(() => {
 		parser(['en_us'])
 	});
 });
 
-test('rejects array arguments', t => {
+test('rejects array argument', t => {
 	t.throws(() => {
 		parser([['250 words per minute', 'en']])
 	});
 });
 
-test('rejects number arguments', t => {
+test('rejects number argument', t => {
 	t.throws(() => {
 		parser([123])
 	});
 });
 
-test('rejects boolean arguments', t => {
+test('rejects boolean argument', t => {
 	t.throws(() => {
 		parser([true])
 	});
 	t.throws(() => {
 		parser([false])
 	});
-});
-
-test('returns arguments as an object', t => {
-	const speedTest = parser(['250 words a minute']);
-	const languageTest = parser(['en']);
-	const objectTest = parser(['1000 characters per hour', 'hi']);
-
-	t.is(typeof objectTest, 'object');
-	t.is(speedTest.speed, '250 words a minute');
-	t.is(languageTest.language, 'en');
-	t.is(objectTest.speed, '1000 characters per hour');
-	t.is(objectTest.language, 'hi');
 });
