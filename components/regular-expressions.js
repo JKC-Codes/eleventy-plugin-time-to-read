5fbe8b7a0df1efccb262e5e595a0d756d8463e63
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-09-29 15:19:58 +0100

    Allow any preposition in speed

--- components/regular-expressions.js
+++ components/regular-expressions.js
@@ -4,14 +4,11 @@ const speedUnitAmount = String.raw`[0-9]+(\.[0-9]+)?`;
 // Regex = 'characters' or 'words'
 const speedUnitMeasure = String.raw`(character|word)s?`;
 
-// Regex = 'per' or 'a' or 'an'
-const speedUnitPreposition = String.raw`(per|a|an)`;
-
 // Regex = 'hour' or 'minute' or 'second'
 const speedUnitInterval = String.raw`(hour|minute|second)s?`;
 
-// Regex = speedUnitAmount + ' ' + speedUnitMeasure + ' ' + optional speedUnitPreposition + speedUnitInterval
-const speed = String.raw`^${speedUnitAmount} ${speedUnitMeasure} (${speedUnitPreposition} )?${speedUnitInterval}$`;
+// Regex = speedUnitAmount + ' ' + speedUnitMeasure + ' ' + optional anything + speedUnitInterval
+const speed = String.raw`^${speedUnitAmount} ${speedUnitMeasure} (.* )*${speedUnitInterval}$`;
 
 // Regex = '<' + optional '/' + 1 or more alphanumeric characters + a non-word character + 0 or more non-'>' characters + '>'
 const htmlTags = String.raw`<\/?[a-z0-9]+\b[^>]*>`;
@@ -24,11 +21,6 @@ const html = String.raw`${htmlTags}|${htmlComments}`;
 
 module.exports = {
 	speedUnitAmount,
-	speedUnitMeasure,
-	speedUnitPreposition,
-	speedUnitInterval,
 	speed,
-	htmlTags,
-	htmlComments,
 	html
 }
