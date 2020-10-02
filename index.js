9f210f733c4a7e059b553fcab3cc9fd333df1c79
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-10-02 02:04:06 +0100

    Rename 'main' entry point to index from .eleventy

---
+++ index.js
@@ -0,0 +1,20 @@
+const defaultOptions = require('./components/options-default.js');
+const validateOptions = require('./components/options-validator.js');
+const parseOptions = require('./components/options-parser.js');
+const measureTime = require('./components/measure-time.js');
+
+module.exports = function(eleventyConfig, customOptions) {
+	const globalOptions = Object.assign(
+		{},
+		defaultOptions,
+		validateOptions(customOptions)
+	);
+	eleventyConfig.addFilter('timeToRead', function(input, ...instanceOptions) {
+		const options = Object.assign(
+			{},
+			globalOptions,
+			validateOptions(parseOptions(instanceOptions))
+		);
+		return measureTime(input, options);
+	});
+}
