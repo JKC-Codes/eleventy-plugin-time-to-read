0358bbd3beb9fdbb8b8fd0579d7b2ccd3fc06420
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-10-11 00:04:24 +0100

    Simplify default speed option

--- components/options-default.js
+++ components/options-default.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// The number of characters read per minute tends to be around 1000 for all languages: https://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
-	speed: '1000 characters per minute',
+	speed: '1000 character minute',
 	language: 'en',
 	style: 'long',
 	type: 'unit',
